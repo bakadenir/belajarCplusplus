25de09cecaae2af2f4d7ac402a69f5c4791f776e
unknown <bakadenir@gmail.com>
2024-10-23 12:08:52 +0700

    Fundamental C++

---
+++ 01_helloWorld.cpp
@@ -0,0 +1,12 @@
+#include <iostream> //Standard Library C++ dan menyediakan fungsi dan objek yang digunakan untuk melakukan input dan output (I/O) dalam bahasa C++
+
+using namespace std; // Library cout, cin, string, vector, dan lain-lain.
+
+
+// Fungsi untuk eksekusi program dimulai
+int main() {
+    cout << "Hello, World!" << endl; // Menampilkan output ke layar, endl untuk garis baru
+    cout << "Mari kita belajar C++ \n"; // \n untuk garis baru juga
+    cout << "Semoga bisa istiqomah";
+    return 0; // Mengembalikan nilai 0, menandakan program berjalan sukses
+}
